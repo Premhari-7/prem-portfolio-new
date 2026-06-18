fbb6dfa2f7f8eb11a339280ca689d10c6aa02299
Prem Hari <haripradeep185@gmail.com>
2026-06-18 18:50:01 +0530

    Fix react-pdf CSS import paths for v10+

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -11,8 +11,8 @@ import {
   HiExternalLink,
 } from "react-icons/hi";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
